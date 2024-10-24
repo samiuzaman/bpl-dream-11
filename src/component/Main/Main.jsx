0139c52c0b412e0957ce59d67bf1639fcfffd79d
samiuzzaman <chowdhurysamiuzzaman@gmail.com>
2024-10-25 02:07:20 +0600

    Created Dynamic Card & Showing Dynamic Data

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,12 +1,17 @@
+import Availabele from "../Availabele/Availabele";
+
 const Main = () => {
   return (
     <div>
       <div className="flex justify-between items-center">
-        <h2>Available Players</h2>
+        <h2 className="text-2xl text-dark1-0 font-bold">Available Players</h2>
         <div className="">
           <button className="btn myBtn btn-color join-item">Available</button>
           <button className="btn border-2 text-dark4-0">Selected (0)</button>
         </div>
+      </div>
+      <div>
+        <Availabele></Availabele>
       </div>
     </div>
   );
