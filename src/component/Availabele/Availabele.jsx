a5b445d7b95bc231eafdf274c857ed87fcbfa762
samiuzzaman <chowdhurysamiuzzaman@gmail.com>
2024-10-25 02:44:00 +0600

    create Dynamic Selected Section

--- src/component/Availabele/Availabele.jsx
+++ src/component/Availabele/Availabele.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { IoIosContact } from "react-icons/io";
 import { FaFlag } from "react-icons/fa";
+import Selected from "../Selected/Selected";
 
 const Availabele = () => {
   const [cards, setCards] = useState([]);
@@ -12,58 +13,61 @@ const Availabele = () => {
   }, []);
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 mt-8">
-      {cards.map((card) => (
-        <div
-          key={card.name}
-          className="card card-compact bg-base-100 w-96 shadow-xl p-4 border-2"
-        >
-          <figure className="">
-            <img
-              className="h-72 w-full rounded-xl"
-              src={card.image}
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <div className="flex gap-2 items-center">
-              <span className="text-3xl">
-                <IoIosContact></IoIosContact>
-              </span>
-              <h3 className="text-xl font-semibold text-dark1-0">
-                {card.name}
-              </h3>
-            </div>
-            <div className="flex justify-between items-center border-b-2 pb-4">
-              <p className="flex items-center gap-2 text-base text-dark3-0">
-                <FaFlag></FaFlag> {card.country}
-              </p>
-              <span className="p-2 text-sm font-medium text-dark1-0 bg-[#1313130D] rounded-md">
-                {card.role}
-              </span>
-            </div>
-            <div className="flex justify-between items-end pt-3">
-              <div>
-                <h6 className="text-base text-dark1-0 font-bold mb-3">
-                  Rating
-                </h6>
-                <h6 className="text-base text-dark1-0 font-bold mb-3">
-                  Left-Hand-Bat
-                </h6>
-                <h6 className="text-base text-dark1-0 font-bold">
-                  Price: ${card.bidding_price}
-                </h6>
+    <div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 mt-8">
+        {cards.map((card) => (
+          <div
+            key={card.name}
+            className="card card-compact bg-base-100 w-96 shadow-xl p-4 border-2"
+          >
+            <figure className="">
+              <img
+                className="h-72 w-full rounded-xl"
+                src={card.image}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <div className="flex gap-2 items-center">
+                <span className="text-3xl">
+                  <IoIosContact></IoIosContact>
+                </span>
+                <h3 className="text-xl font-semibold text-dark1-0">
+                  {card.name}
+                </h3>
               </div>
-              <div className="text-right">
-                <h6 className="mb-3">{card.batting_bowling_type}</h6>
-                <button className="border-2 p-2 rounded-lg">
-                  Choose Player
-                </button>
+              <div className="flex justify-between items-center border-b-2 pb-4">
+                <p className="flex items-center gap-2 text-base text-dark3-0">
+                  <FaFlag></FaFlag> {card.country}
+                </p>
+                <span className="p-2 text-sm font-medium text-dark1-0 bg-[#1313130D] rounded-md">
+                  {card.role}
+                </span>
+              </div>
+              <div className="flex justify-between items-end pt-3">
+                <div>
+                  <h6 className="text-base text-dark1-0 font-bold mb-3">
+                    Rating
+                  </h6>
+                  <h6 className="text-base text-dark1-0 font-bold mb-3">
+                    Left-Hand-Bat
+                  </h6>
+                  <h6 className="text-base text-dark1-0 font-bold">
+                    Price: ${card.bidding_price}
+                  </h6>
+                </div>
+                <div className="text-right">
+                  <h6 className="mb-3">{card.batting_bowling_type}</h6>
+                  <button className="border-2 p-2 rounded-lg">
+                    Choose Player
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <Selected cards={cards}></Selected>
     </div>
   );
 };
---
+++ src/component/Selected/Selected.jsx
@@ -0,0 +1,37 @@
+import { FaTrashAlt } from "react-icons/fa";
+const Selected = ({ cards }) => {
+  console.log(cards);
+  return (
+    <div className="mt-12">
+      <h2>Selected Page</h2>
+      <div>
+        {cards.map((card) => (
+          <div
+            key={card.id}
+            className=" flex justify-between items-center border-2 p-4 rounded-xl mb-6"
+          >
+            <div className="flex items-center gap-5">
+              <img className="w-20 h-20 rounded-lg" src={card.image}></img>
+              <div>
+                <h2 className="text-xl font-bold text-dark1-0 mb-1">
+                  {card.name}
+                </h2>
+                <p className="text-base font-normal text-dark4-0">
+                  {card.batting_bowling_type}
+                </p>
+              </div>
+            </div>
+            <button className="btn hover:bg-btnBg-0 text-red-600 text-2xl">
+              <FaTrashAlt></FaTrashAlt>
+            </button>
+          </div>
+        ))}
+        <div className="w-48 p-2 border-2 rounded-xl">
+          <button className="w-full btn myBtn btn-color">Add More Player</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Selected;
